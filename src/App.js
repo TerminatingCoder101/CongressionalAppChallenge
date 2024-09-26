e73621325e2b7e7bcf30fd60f5a17d268be83574
knit715 <knit715@gmail.com>
2024-09-25 17:06:32 -0700

    changing home page to hero page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Hero from './pages/Hero';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} />
         {/* Add more routes here for other pages */}
       </Routes>
     </Router>
